5323892c61fda405fe03202922bafebc1549b9b7
Tony Nsang <nsan0004@algonquinlive.com>
2024-03-17 22:46:43 -0400

    Added some javadocs comments

--- app/src/main/java/algonquin/cst2335/final_project_w24/DictionaryActivity.java
+++ app/src/main/java/algonquin/cst2335/final_project_w24/DictionaryActivity.java
@@ -8,13 +8,37 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.widget.EditText;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import algonquin.cst2335.final_project_w24.databinding.ActivityDictionaryBinding;
 
+/**
+ * This class runs the Dictionary Application where a word is entered and on click of search
+ * It redirects the user to an API and retrieves the matching definitions of that word
+ * @author Tony Nsang
+ *
+ */
 public class DictionaryActivity extends AppCompatActivity {
+    /**
+     * Viewbinding variable
+     */
     ActivityDictionaryBinding binding;
-
+    /**
+     * SharedPreferences File name static variable
+     */
     private static final String SHARED_PREFS_FILE = "MyData";
-
+    /**
+     * Array to store definitions of a word
+     */
+    private List<String> definitions = new ArrayList<>();
+    /**
+     * On create function to load the Activities layout inclusing widgets
+     * @param savedInstanceState If the activity is being re-initialized after
+     *     previously being shut down then this Bundle contains the data it most
+     *     recently supplied in {@link #onSaveInstanceState}.  <b><i>Note: Otherwise it is null.</i></b>
+     *
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +60,11 @@ public class DictionaryActivity extends AppCompatActivity {
 
 
     }
+
+    /**
+     * Redirects user to Dictionary API and fetches the definitions related to the word entered by user
+     * @param word Word entered by user
+     */
     private void fetchDefinitions(String word) {
         String url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + word;
 
