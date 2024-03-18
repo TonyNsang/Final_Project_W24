8819ea68447bb7db7cdf038019684c00d821612f
Tony Nsang <nsan0004@algonquinlive.com>
2024-03-17 21:55:55 -0400

    Created Tony Branch and Created layout for Dictionary layout

---
+++ app/src/main/java/algonquin/cst2335/final_project_w24/DictionaryActivity.java
@@ -0,0 +1,14 @@
+package algonquin.cst2335.final_project_w24;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DictionaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dictionary);
+    }
+}
