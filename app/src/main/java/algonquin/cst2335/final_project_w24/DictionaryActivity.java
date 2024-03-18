ba4d4c77c46059f10dfb0c2731a090d769e2a6e4
Tony Nsang <nsan0004@algonquinlive.com>
2024-03-17 22:29:29 -0400

    Added SharedPreferences for the Search word

--- app/src/main/java/algonquin/cst2335/final_project_w24/DictionaryActivity.java
+++ app/src/main/java/algonquin/cst2335/final_project_w24/DictionaryActivity.java
@@ -2,17 +2,43 @@ package algonquin.cst2335.final_project_w24;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.widget.EditText;
 
 import algonquin.cst2335.final_project_w24.databinding.ActivityDictionaryBinding;
 
 public class DictionaryActivity extends AppCompatActivity {
     ActivityDictionaryBinding binding;
+
+    private static final String SHARED_PREFS_FILE = "MyData";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         binding = ActivityDictionaryBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        SharedPreferences prefs = getSharedPreferences(SHARED_PREFS_FILE, Context.MODE_PRIVATE);
+        String lastSearchTerm = prefs.getString("last_search_term", "");
+        binding.searchText.setText(lastSearchTerm);
+
+        binding.searchButton.setOnClickListener(click -> {
+            String word = binding.searchText.getText().toString();
+            fetchDefinitions(word);
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putString("last_search_term", word);
+            editor.apply();
+        });
+
+
+    }
+    private void fetchDefinitions(String word) {
+        String url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + word;
+
     }
 }
+
