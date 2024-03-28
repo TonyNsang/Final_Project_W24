c96e48aa969139f5963ea0f3f229075271706431
Tony Nsang <nsan0004@algonquinlive.com>
2024-03-28 16:07:00 -0400

    Added Toolbar To DictionaryAPI

--- app/src/main/java/algonquin/cst2335/final_project_w24/Dictionary/DictionaryActivity.java
+++ app/src/main/java/algonquin/cst2335/final_project_w24/Dictionary/DictionaryActivity.java
@@ -1,7 +1,9 @@
 package algonquin.cst2335.final_project_w24.Dictionary;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -9,17 +11,23 @@ import androidx.recyclerview.widget.RecyclerView.Adapter;
 
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.NetworkError;
+import com.android.volley.ParseError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.ServerError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -31,7 +39,11 @@ import org.w3c.dom.Text;
 import java.util.ArrayList;
 import java.util.List;
 
+import algonquin.cst2335.final_project_w24.DeezerApp.SongActivity;
+import algonquin.cst2335.final_project_w24.MainActivity;
 import algonquin.cst2335.final_project_w24.R;
+import algonquin.cst2335.final_project_w24.Recipe.RecipeActivity;
+import algonquin.cst2335.final_project_w24.SunApp.SunActivity;
 import algonquin.cst2335.final_project_w24.databinding.ActivityDictionaryBinding;
 
 /**
@@ -80,24 +92,26 @@ public class DictionaryActivity extends AppCompatActivity {
         model = new ViewModelProvider(this).get(DictionaryViewModel.class);
 
         binding = ActivityDictionaryBinding.inflate(getLayoutInflater());
-        requestQueue = Volley.newRequestQueue(this);
+
 
         setContentView(binding.getRoot());
 
+        Toolbar toolbar = binding.myToolbar;
+        setSupportActionBar(toolbar);
 
         binding.dictionaryView.setLayoutManager(new LinearLayoutManager(this));
 
 
-
+        //Shared Preferences for Search Term in edit text
         SharedPreferences prefs = getSharedPreferences(SHARED_PREFS_FILE, Context.MODE_PRIVATE);
         String lastSearchTerm = prefs.getString("last_search_term", "");
         binding.searchText.setText(lastSearchTerm);
 
-
+        //OnClick listener for search button
         binding.searchButton.setOnClickListener(click -> {
             model.searchText.postValue( binding.searchText.getText().toString());
 
-//            String word = binding.searchText.getText().toString();
+
             String word = binding.searchText.getText().toString();
             SharedPreferences.Editor editor = prefs.edit();
             editor.putString("last_search_term", word);
@@ -158,42 +172,59 @@ public class DictionaryActivity extends AppCompatActivity {
      * @param word Word entered by user
      */
     private void fetchDefinitions(String word) {
+        requestQueue = Volley.newRequestQueue(this);
         String url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + word;
-        // Create the JSON request
+
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
                 response -> {
                     try {
-
-                        JSONArray definitionsArray = response.getJSONArray("meanings");
-                        definitions.clear();
-                        for (int i = 0; i < definitionsArray.length(); i++) {
-                            JSONObject meaningObject = definitionsArray.getJSONObject(i);
-                            JSONArray definitionsJsonArray = meaningObject.getJSONArray("definitions");
-                            for (int j = 0; j < definitionsJsonArray.length(); j++) {
-                                JSONObject definitionObject = definitionsJsonArray.getJSONObject(j);
-                                String definition = definitionObject.getString("definition");
-                                ArrayList<String> array = new ArrayList<>();
-                                array.add(definition);
-
-
-
-                                DictionaryData data = new DictionaryData();
-                                data.setSearchTerm(binding.searchText.getText().toString());
-                                data.setDefinitionsOfTerm(array);
-
-                                definitions.add(data);
+                        JSONArray jsonArray = new JSONArray(word);
+                        List<DictionaryData> wordDefinitions = new ArrayList<>();
+                        for (int i = 0; i < jsonArray.length(); i++) {
+                            JSONObject wordObject = jsonArray.getJSONObject(i);
+                            JSONArray meaningsArray = wordObject.getJSONArray("meanings");
+                            for (int j = 0; j < meaningsArray.length(); j++) {
+                                JSONObject meaningObject = meaningsArray.getJSONObject(j);
+                                JSONArray definitionsArray = meaningObject.getJSONArray("definitions");
+                                for (int k = 0; k < definitionsArray.length(); k++) {
+                                    JSONObject definitionObject = definitionsArray.getJSONObject(k);
+                                    String definition = definitionObject.optString("definition");
+                                    // Create a DictionaryData object to store the word and its definition
+                                    DictionaryData data = new DictionaryData();
+                                    data.setSearchTerm(word);
+                                    ArrayList<String> definitionList = new ArrayList<>();
+                                    definitionList.add(definition);
+                                    data.setDefinitionsOfTerm(definitionList);
+                                    // Add the DictionaryData object to the list
+                                    wordDefinitions.add(data);
+                                }
                             }
                         }
-                        myAdapter.notifyDataSetChanged();
+                        // Now you have a list of DictionaryData objects containing the word and its definitions
+                        // Do whatever you need with this data
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
                 },
-                error -> Toast.makeText(DictionaryActivity.this, "Error fetching definitions", Toast.LENGTH_SHORT).show());
+                error -> {
+                    if (error instanceof NetworkError) {
+                        Toast.makeText(DictionaryActivity.this, "Network error", Toast.LENGTH_SHORT).show();
+                    } else if (error instanceof ServerError) {
+                        Toast.makeText(DictionaryActivity.this, "Server error", Toast.LENGTH_SHORT).show();
+                    } else if (error instanceof ParseError) {
+                        Toast.makeText(DictionaryActivity.this, "Parse error", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(DictionaryActivity.this, "Unknown error", Toast.LENGTH_SHORT).show();
+                    }
+                    error.printStackTrace();
+                }
+        );
 
         // Add the request to the RequestQueue
         requestQueue.add(jsonObjectRequest);
     }
+
+
 
     /**
      * an object for representing everything that goes on a row in the list
@@ -214,6 +245,46 @@ public class DictionaryActivity extends AppCompatActivity {
             formattedDefinitions.append("\u2022 ").append(definition).append("\n"); // Unicode for bullet point
         }
         return formattedDefinitions.toString();
+    }
+
+    //Toolbar Menu items
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.my_menu, menu);
+        super.onCreateOptionsMenu(menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.dictionaryIcon) {
+            Intent dictionaryPage = new Intent(DictionaryActivity.this, DictionaryActivity.class);
+            startActivity(dictionaryPage);
+            return true;
+        } else if (item.getItemId() == R.id.recipeIcon) {
+            Intent recipePage = new Intent(DictionaryActivity.this, RecipeActivity.class);
+            startActivity(recipePage);
+            return true;
+        } else if (item.getItemId() == R.id.sunIcon) {
+            Intent sunPage = new Intent(DictionaryActivity.this, SunActivity.class);
+            startActivity(sunPage);
+            return true;
+
+        } else if (item.getItemId() == R.id.help_Icon) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Help");
+            builder.setMessage("Instructions for using the Dictionary API:\n\n" +
+                    "1. Enter a word to look up the definition.\n" +
+                    "2. Definitions will be displayed in a RecyclerView.\n" +
+                    "3. You can save search terms and definitions locally.\n" +
+                    "4. Use the 'View Saved Terms' button to view and delete saved terms.\n" +
+                    "5. SharedPreferences will save your last search term.");
+            builder.setPositiveButton("OK", ((dialog, click) ->{} )).create().show();
+            return true;
+        }else {
+
+        }
+        return false;
     }
 }
 
